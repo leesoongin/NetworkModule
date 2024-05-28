ad903a289cb8350a429d59811838047ac1820f12
SoongInLee <tnddls2ek@naver.com>
2024-05-28 13:06:50 +0900

    NetworkBuilder Public Protocol 정의 및 Default Value 세팅

--- Sources/NetworkModule/NetworkModule.swift
+++ Sources/NetworkModule/NetworkModule.swift
@@ -1,2 +1,33 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+import Alamofire
+
+//MARK: - Define
+public protocol NetworkBuilder {
+    associatedtype ResponseType
+    
+    var method: HTTPMethod { get }
+    var header: HTTPHeader? { get }
+    var baseURL: String { get }
+    var path: String { get }
+    var parameters: Parameters? { get }
+    var parameterEncoding: ParameterEncoding { get }
+    var networkSession: Session { get }
+    
+    func decode(from data: Data) throws -> ResponseType
+}
+
+//MARK: - Default Value Setting
+extension NetworkBuilder {
+    var header: HTTPHeader? { nil }
+    var parameterEncoding: ParameterEncoding {
+        switch method {
+        case .get:
+            URLEncoding.default
+        default:
+            JSONEncoding.default
+        }
+    }
+}
