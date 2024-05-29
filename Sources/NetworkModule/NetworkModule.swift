412141b4f5f7036d510caf6efd630b3bc2cced32
SoongInLee <tnddls2ek@naver.com>
2024-05-29 16:28:22 +0900

    Network Builder 기능 추가
    - HTTPHeader 기본값 설정 추가, 기본 헤더에 추가할 수 있도록 additionalHeader 변수 추가
    - JSON Decoder를 이용한 decode 함수 구현
    - API Response decode 함수 구현

--- Sources/NetworkModule/NetworkModule.swift
+++ Sources/NetworkModule/NetworkModule.swift
@@ -4,30 +4,69 @@
 import Foundation
 import Alamofire
 
+public typealias AdditionalHeader = [String: String]
+
 //MARK: - Define
 public protocol NetworkBuilder {
-    associatedtype ResponseType
+    associatedtype ResponseType: Decodable
     
     var method: HTTPMethod { get }
-    var header: HTTPHeader? { get }
+    var header: HTTPHeaders? { get }
     var baseURL: String { get }
     var path: String { get }
     var parameters: Parameters? { get }
     var parameterEncoding: ParameterEncoding { get }
     var networkSession: Session { get }
+    var additionalHeader: AdditionalHeader? { get }
     
     func decode(from data: Data) throws -> ResponseType
 }
 
 //MARK: - Default Value Setting
 extension NetworkBuilder {
-    var header: HTTPHeader? { nil }
+    var header: HTTPHeaders {
+        var defaultHeader: HTTPHeaders = .default
+
+        header?.makeIterator().forEach {
+            defaultHeader.update($0)
+        }
+        
+        additionalHeader?.forEach { (key, value) in
+            defaultHeader.update(name: key, value: value)
+        }
+        
+        return defaultHeader
+    }
     var parameterEncoding: ParameterEncoding {
         switch method {
         case .get:
             URLEncoding.default
         default:
             JSONEncoding.default
+        }
+    }
+}
+
+extension NetworkBuilder {
+    func decode(from data: Data) throws -> ResponseType {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        
+        return try decoder.decode(ResponseType.self, from: data)
+    }
+}
+
+extension NetworkBuilder {
+    func decodeResponse(debug: Bool, response: HTTPURLResponse, data: Data) throws -> ResponseType {
+        if debug {
+            print("Network -> response : \(response)")
+            print("Network -> \(String(decoding: data, as: UTF8.self))")
+        }
+        
+        do {
+            return try decode(from: data)
+        } catch {
+            throw error
         }
     }
 }
